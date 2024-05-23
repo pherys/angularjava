9247764eb808ef6755ace71f7660eb46d4b94129
pherys <fernandabrito.linx@gmail.com>
2024-05-23 08:52:52 -0300

    first com imagem quebrada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent,
+  }
+];
